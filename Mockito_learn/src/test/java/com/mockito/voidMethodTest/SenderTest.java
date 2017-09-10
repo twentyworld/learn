34a3e53c9cca7dc361af3ca32203173c3b74b0bf
twentyworld <1394760588@qq.com>
2017-09-10 17:47:50 +0800

    solve the question about a method of the object invokes a void method of it's attribute. such as:     public void initialize() {         database.addListener(new ArticleListener());     }

--- Mockito_learn/src/test/java/com/mockito/voidMethodTest/SenderTest.java
+++ Mockito_learn/src/test/java/com/mockito/voidMethodTest/SenderTest.java
@@ -1,44 +1,38 @@
 package com.mockito.voidMethodTest; 
 
-
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import org.junit.Test; 
+
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.Before; 
 import org.junit.After;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 /** 
-* Sender Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>���� 10, 2017</pre> 
-* @version 1.0 
-*/ 
-public class SenderTest { 
+* Sender Tester.
+* @author <Authors name>
+* @since <pre>���� 10, 2017</pre>
+        * @version 1.0
+        */
 
+public class SenderTest {
+    @Mock private Emailer emailer;
+    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @InjectMocks private Sender sender;
 
-    Sender sender
-@Before
-public void before() throws Exception {
-        sender = new Sender();
-} 
+    @Before public void before() throws Exception { }
 
-@After
-public void after() throws Exception { 
-} 
-
-/** 
-* 
-* Method: send() 
-* 
-*/ 
-@Test
-public void testSend() throws Exception { 
-
-    Emailer emailer = Mockito.mock(Emailer.class);
-    when(emailer.send(any())).thenReturn();
-} 
-
-
-} 
+    @After public void after() throws Exception { }
+    /**
+     * Method: send()
+     */
+    @Test
+    public void testSend() throws Exception {
+        sender.send();
+        verify(emailer).send(anyString());
+    }
+}
