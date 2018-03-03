67c53cfa5b30bb96a9a780410d47eace9d2e044a
twentyworld <1394760588@qq.com>
2018-03-03 17:29:55 +0800

    add some demo code of concurrent packages java util.

---
+++ JDKlearn/src/main/java/com/concurrent/SemaphoreLearn.java
@@ -0,0 +1,33 @@
+package com.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by teemper on 2018/3/3, 17:13.
+ *
+ * @auther Zed.
+ * copy as you like, but with these words.
+ * from win.
+ */
+public class SemaphoreLearn {
+
+    public static void main(String[] args) {
+
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        Semaphore semaphore = new Semaphore(3);
+
+        for(int i = 0;i<10;i++) {
+            new Thread(new SemaphoreThread(countDownLatch,semaphore)).start();
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("all worker has finish the work");
+
+    }
+
+}
---
+++ JDKlearn/src/main/java/com/concurrent/SemaphoreThread.java
@@ -0,0 +1,50 @@
+package com.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by teemper on 2018/3/3, 17:19.
+ *
+ * @auther Zed.
+ * copy as you like, but with these words.
+ * from win.
+ */
+public class SemaphoreThread implements Runnable {
+
+    private CountDownLatch countDownLatch;
+    private Semaphore semaphore;
+
+    public SemaphoreThread(CountDownLatch countDownLatch, Semaphore semaphore) {
+        this.countDownLatch = countDownLatch;
+        this.semaphore = semaphore;
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+            System.out.println("thread: " +Thread.currentThread().getName()+"is asking a new acquirement.");
+            Thread.sleep(1000);
+            System.out.println("thread: " +Thread.currentThread().getName()+"release the acquirement.");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            countDownLatch.countDown();
+            semaphore.release();
+        }
+
+    }
+}
