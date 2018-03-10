c7738d34c48a8e669c98ddcef488253852428a66
twentyworld <li930525>
2018-03-10 23:38:57 +0800

    add some Hibernate demos.

---
+++ Spring-Data/HibernateLearn/src/main/java/com/learn/EmployeeManager.java
@@ -0,0 +1,123 @@
+package com.learn;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class EmployeeManager {
+    private static SessionFactory factory;
+
+
+    public static void main(String[] args) {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setInterceptor(new EntityInterceptor());
+            factory = configuration.configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        EmployeeManager ME = new EmployeeManager();
+
+        /* Add few employee records in database */
+        Integer empID1 = ME.addEmployee("Zara", "Ali", 1000);
+        Integer empID2 = ME.addEmployee("Daisy", "Das", 5000);
+        Integer empID3 = ME.addEmployee("John", "Paul", 10000);
+
+        /* List down all the employees */
+        ME.listEmployees();
+
+        /* Update employee's records */
+        ME.updateEmployee(empID1, 5000);
+
+        /* Delete an employee from the database */
+        ME.deleteEmployee(empID2);
+
+        /* List down new list of the employees */
+        ME.listEmployees();
+    }
+
+
+    /* Method to CREATE an employee in the database */
+    public Integer addEmployee(String fname, String lname, int salary) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer employeeID = null;
+        try {
+            tx = session.beginTransaction();
+            Employee employee = new Employee(fname, lname, salary);
+            employeeID = (Integer) session.save(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return employeeID;
+    }
+
+    /* Method to  READ all the employees */
+    public void listEmployees() {
+        System.out.println("88****************************************listEmployees****************************************");
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            List<Employee> employees = session.createQuery("FROM Employee").list();
+            for (Employee employee: employees){
+                System.out.println(employee);
+                employee.setLastName("ttt");
+            }
+
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    /* Method to UPDATE salary for an employee */
+    public void updateEmployee(Integer EmployeeID, int salary) {
+        System.out.println("88****************************************updateEmployee****************************************");
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Employee employee = session.get(Employee.class, EmployeeID);
+            employee.setSalary(salary);
+            session.update(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    /* Method to DELETE an employee from the records */
+    public void deleteEmployee(Integer EmployeeID) {
+        System.out.println("88****************************************deleteEmployee****************************************");
+
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Employee employee =
+                    session.get(Employee.class, EmployeeID);
+            session.delete(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+}
