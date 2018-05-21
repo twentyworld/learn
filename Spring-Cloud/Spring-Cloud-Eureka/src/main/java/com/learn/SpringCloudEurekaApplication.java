567fdef485e107f3c53208424485b2f424dabdfc
twentyworld <1394760588@qq.com>
2018-05-22 00:19:39 +0800

    change file encoding.

--- Spring-Cloud/Spring-Cloud-Eureka/src/main/java/com/learn/SpringCloudEurekaApplication.java
+++ Spring-Cloud/Spring-Cloud-Eureka/src/main/java/com/learn/SpringCloudEurekaApplication.java
@@ -1,20 +1,1 @@
-package com.learn;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-/**
- * Created by teemper on 2017/12/3, 18:37.
- *
- * @auther Zed.
- * copy as you like, but with these words.
- * from win.
- */
-@SpringBootApplication
-@EnableEurekaServer
-public class SpringCloudEurekaApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudEurekaApplication.class,args);
-    }
-}
+package com.learn;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;/** * Created by teemper on 2017/12/3, 18:37. * * @auther Zed. * copy as you like, but with these words. * from win. */@SpringBootApplication@EnableEurekaServerpublic class SpringCloudEurekaApplication {    public static void main(String[] args) {        SpringApplication.run(SpringCloudEurekaApplication.class,args);    }}
